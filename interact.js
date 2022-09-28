38670b8a25e09b4224547514758783eed4a338f4
Jaskirat Singh <jaskiratsingh772@gmail.com>
2022-09-28 04:45:32 -0400

    interac completed

--- interact.js
+++ interact.js
@@ -1,7 +1,7 @@
 
 
 let balance = {
-    "balance1": 2000,
+    "balance1": 5000,
     
     "balance2": 500,
 }
@@ -16,25 +16,68 @@ let balanceChanged = () => {
 
    
 
-    let balanceElement1 = document.getElementById("balance1");
-    balanceElement1.innerHTML = `Balance: $${balance.balance1}`;
+    let balanceElement1 = document.getElementById("account").getElementsByTagName("option");
+   
+    balanceElement1[0].innerHTML = `<h3> Chequing </h3> <p> Balance: $${balance.balance1} </p>`;
 
-    let balanceElement2 = document.getElementById("balance2");
-    balanceElement2.innerHTML = `Balance: $${balance.balance2}`;
+    balanceElement1[1].innerHTML =`<h3> Saving </h3> <p> Balance: $${balance.balance2} </p>`;;
 }
 
-let sendMoney = (amount) => {
+let sendMoney = () => {
+
+    let amount = document.getElementById("amount").value;
+    amount = parseInt(amount);
+    let email = document.getElementById("email").value;
+    email = email.toLowerCase();
+    let message = document.getElementById("message").value;
+    let securityQuestion = document.getElementById("securityQuestion").value;
+    securityQuestion = securityQuestion.toLowerCase();
+    let securityAnswer = document.getElementById("securityAnswer").value;
+    securityAnswer = securityAnswer.toLowerCase();
+
+    if((securityQuestion == "") && (securityAnswer == "") || (securityQuestion && securityAnswer)){ 
+    
+        if( isNaN(amount) || amount <= 0){
+            alert("Please enter an amount");
+            return false;
+        }
+
+
+        if(email == "" ){
+            alert("Please enter an email");
+            return false;
+        }
+
+    if (amount > balance[`balance${selectedAccount}`]) {
+        alert("Insufficient funds");
+    }
+    else{
+        balance[`balance${selectedAccount}`] -= amount;
+
+        alert(`$${amount} has been sent to ${email}`);
+
+        document.getElementById("amount").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("message").value = "";
+        document.getElementById("securityQuestion").value = "";
+        document.getElementById("securityAnswer").value = "";
+        balanceChanged();
+    }
+
+    }else{
+        if(securityQuestion == ""){
+            alert("Please enter a security question");
+            
+        }
+        else if(securityAnswer == ""){
+            alert("Please enter a security answer");
+        }
+        return false;
+    }
 
     
 
-
-    if (selectedAccount == 1) {
-       balance.balance1 = 0;
-       
-    } else {
-        balance.balance1 += amount;
-      
-    }
+  
     balanceChanged();
 }
 
@@ -43,10 +86,11 @@ let account = document.getElementById("account");
 account.addEventListener("change", (event) => {
     selectedAccount = event.target.value;
     selectedAccount = parseInt(selectedAccount);
-    console.log(selectedAccount);
 }
 )
 
 
+window.onload = function(){
+    balanceChanged();
+}
 
-//balanceChanged();
